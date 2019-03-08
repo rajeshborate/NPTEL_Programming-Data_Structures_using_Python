3082df17dd04b3ab2ac043e81520990b158dce19
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-08 18:17:04 +0530

    Update week_02_Programming_Assignment.py

--- week_02_Programming_Assignment.py
+++ week_02_Programming_Assignment.py
@@ -37,5 +37,34 @@ def nestingdepth(s):
     if(s == "a)*(?"):
         max = -1
     return max
+def rotatelist(l, y):
+   if len(l) == 0:
+      return l
+   y = y % len(l)    
+
+   return l[y:] + l[:y]
+import ast
+
+def parse(inp):
+  inp = ast.literal_eval(inp)
+  return (inp)
+
+fncall = input()
+lparen = fncall.find("(")
+rparen = fncall.rfind(")")
+fname = fncall[:lparen]
+farg = fncall[lparen+1:rparen]
+
+if fname == "primepartition":
+  arg = parse(farg)
+  print(primepartition(arg))
+
+if fname == "nestingdepth":
+  arg = parse(farg)
+  print(nestingdepth(arg))
+
+if fname == "rotatelist":
+  (arg1,arg2) = parse(farg)
+  print(rotatelist(arg1,arg2))
 
 
